d3762eb39654f323f9e69b56cdc37bdc012b92d7
eljetto <eljetto@gmail.com>
2014-12-09 14:41:18 +0200

    modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/beepteam/truemetronome/SimpleMetronomeActivity.java 	modified:   app/src/main/java/com/beepteam/truemetronome/SoundManager.java

--- app/src/main/java/com/beepteam/truemetronome/SimpleMetronomeActivity.java
+++ app/src/main/java/com/beepteam/truemetronome/SimpleMetronomeActivity.java
@@ -6,20 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 /*
  * Created by a.sergienko
  */
 public class SimpleMetronomeActivity extends Activity {
     private Boolean isRunning = false;
-    private Bar bar = new Bar(120,Constants.DEFAULT_TIME_SIGNATURE);
+    private BarAudioTrack bar;
     private MetronomeAsyncTask task = new MetronomeAsyncTask();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_metronome);
-
-
         final Button button = (Button) findViewById(R.id.SS_BTN);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -35,19 +35,35 @@ public class SimpleMetronomeActivity extends Activity {
                 }
             }
         });
+        try {
+            bar = new BarAudioTrack(160,Constants.DEFAULT_TIME_SIGNATURE,this.getApplicationContext());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private class MetronomeAsyncTask extends AsyncTask<Bar,Void,String> {
+    private class MetronomeAsyncTask extends AsyncTask<BarAudioTrack,Void,String> {
         MetronomeAsyncTask() {
 
         }
         @Override
-        protected String doInBackground(Bar... bars) {
+        protected String doInBackground(BarAudioTrack... bars) {
+            long before;
+            long after;
+            long maxDelay = Integer.MIN_VALUE;
+            long minDelay = Integer.MAX_VALUE;
+            long delay = 0;
             while(isRunning){
-                for (Bar bar : bars) {
+                for (BarAudioTrack bar : bars) {
+                    before = System.currentTimeMillis();
                     bar.play();
+                    after = System.currentTimeMillis();
+                    delay = after - before - bar.getPause();
+                    if(delay < minDelay) minDelay = (delay);
+                    else if(delay > maxDelay) maxDelay = delay;
                 }
             }
+            System.out.println("MAX DELAY" + maxDelay + "   MIN DELAY" + minDelay);
             return null;
         }
         public void stop() {
